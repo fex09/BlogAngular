1ff54f622e7775cafd2743ca6c4a46d3fd393710
Fénix Solís <fsolisg@gmail.com>
2019-11-27 20:08:51 -0600

    Cambios con servicios REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './components/main/app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
@@ -11,6 +12,8 @@ import { PostAddComponent } from './components/posts/post-add/post-add.component
 import { UsersAddComponent } from './components/users/users-add/users-add.component';
 import { PostEditComponent } from './components/posts/post-edit/post-edit.component';
 import { UsersEditComponent } from './components/users/users-edit/users-edit.component';
+import { PhotosListComponent } from './components/photos/photos-list/photos-list.component';
+import { PhotosAddComponent } from './components/photos/photos-add/photos-add.component';
 
 @NgModule({
   declarations: [
@@ -22,12 +25,15 @@ import { UsersEditComponent } from './components/users/users-edit/users-edit.com
     PostAddComponent,
     UsersAddComponent,
     PostEditComponent,
-    UsersEditComponent
+    UsersEditComponent,
+    PhotosListComponent,
+    PhotosAddComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/photos.service.ts
@@ -0,0 +1,23 @@
+import { Photo } from './../models/photo';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotosService {
+
+  private endpoint = 'https://jsonplaceholder.typicode.com';
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getList(): Observable<any> {
+    return this.http.get(this.endpoint + '/photos');
+  }
+
+  public add(photo: Photo): Observable<Photo> {
+    return this.http.post(this.endpoint + '/photos', photo) as Observable<Photo>;
+  }
+}
